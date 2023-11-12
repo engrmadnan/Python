701982125ec5b0b1598ad74d565c4b97426359fe
engrmadnan <149943630+engrmadnan@users.noreply.github.com>
2023-11-12 18:54:53 +0500

    Add files via upload

--- Adnan/Percentage.py
+++ Adnan/Percentage.py
@@ -5,13 +5,13 @@ if(a>=80 and a<=100):
         print("Distinction")
     else:
         print("")
-elif(a<=70 and a>=70):
+elif(a<80 and a>=70):
     print("A")
-elif(a<=69 and a>=60):
+elif(a<70 and a>=60):
     print("B")
-elif(a<=59 and a>=50):
+elif(a<60 and a>=50):
     print("C")
-elif(a<=49 and a>=40):
+elif(a<50 and a>=40):
     print("D")
 elif(a>100 or a<0):
     print("Enter Correct Value")
