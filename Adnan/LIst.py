1bccb85fd78161b4dbe0799fa01964a358ddeb05
engrmadnan <149943630+engrmadnan@users.noreply.github.com>
2023-11-19 19:07:48 +0500

    Add files via upload

---
+++ Adnan/LIst.py
@@ -0,0 +1,15 @@
+'''
+a=["Banana", "Chery", "Apple",78.8, 78.8]
+print(a)
+
+print(a[0])
+print(a[3])
+print(len(a)) '''
+
+a=["Saad", "Hasan", "Ahmed", "Raza", 6]
+b=["Ali", "Haseeb", "Faizan", "Ahsan", 4]
+print(a,b)
+
+a[0]="Saeed"
+
+print(a+b)
